330ba055d0afe795846dad139d439a7e9531aade
Boyko03 <boyko100@abv.bg>
2021-02-19 22:52:43 +0200

    Added register method

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 
 from database import db_session, init_db
-# from login import login_manager
-# from models import
+from login import login_manager
+from models import User
 
 from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = "ssucuuh398nuwetubr33rcuhne"
-# login_manager.init_app(app)
+login_manager.init_app(app)
 init_db()
 
 
@@ -29,3 +29,23 @@ def shutdown_context(exception=None):
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if 'login_id' in current_user.__dict__:
+        return redirect(url_for('home'))
+
+    if request.method == 'GET':
+        return render_template("register.html")
+    else:
+        username = request.form['username']
+        email = request.form['email']
+        password = generate_password_hash(request.form['password'])
+
+        user = User(username=username, email=email, password=password)
+
+        db_session.add(user)
+        db_session.commit()
+
+        return redirect(url_for('login'))
