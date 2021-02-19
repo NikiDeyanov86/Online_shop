0d3322d1907b7e329cca1dfc0d569afc7296e336
Boyko03 <boyko100@abv.bg>
2021-02-19 22:51:26 +0200

    Redefined login_required to support user roles

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
 from flask import redirect, url_for
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
 from models import User
 
 login_manager = LoginManager()
@@ -13,3 +13,17 @@ def load_user(user_id):
 @login_manager.unauthorized_handler
 def unauthorized():
     return redirect(url_for('login'))
+
+
+def login_required(role="basic"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+            if not current_user.is_authenticated():
+                return login_manager.unauthorized()
+            urole = login_manager.reload_user().get_role()
+            if urole != role:
+                return login_manager.unauthorized()
+            return fn(*args, **kwargs)
+        return decorated_view
+    return wrapper
