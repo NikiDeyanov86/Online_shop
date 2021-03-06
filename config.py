6bbd244d9f95a968de7264912a5ce0c4afe4d99c
Boyko03 <boyko100@abv.bg>
2021-03-06 17:39:24 +0200

    Changed super admin password

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+from werkzeug.security import generate_password_hash
+
+
 SECRET_KEY = "ssucuuh398nuwetubr33rcuhne"
 EMAIL = "boyko.georgiev03@gmail.com"
-PASSWORD = "8C6976E5B5410415BDE908BD4DEE15DFB167A9C873FC4BB8A81F6F2AB448A918"
+PASSWORD = generate_password_hash("admin")
